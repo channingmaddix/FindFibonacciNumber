e7dd00d53585e7b74d2642872ace002a2400d7a6
channingmaddix <52470182+channingmaddix@users.noreply.github.com>
2019-08-08 22:48:21 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ namespace FindFibonacciNumber
 
         static void FindFibonacciNumber(int n)
         {
-            if(n == 1)
-            {
-                Console.WriteLine("places 1 and 2");
+            if(n == 1)                                     //Kind of cheating, these first three if statements check if 
+            {                                              //'n' is in one of the first 3 places of the sequence and writes
+                Console.WriteLine("places 1 and 2");       //the place of the number.
             }
             if(n == 2)
             {
@@ -23,16 +23,16 @@ namespace FindFibonacciNumber
             {
                 Console.WriteLine("place #4");
             }
-            int[] fib = new int[n];
-            fib[0] = 1;
-            fib[1] = 1;
+            int[] fib = new int[n];                         //Because we don't know how long the array needs to be for any
+            fib[0] = 1;                                     //given number 'n' until we write it out, the array is intialized
+            fib[1] = 1;                                     //with a length of 'n', to be safe.
             int i = 1;
-            while (fib[i] < n)
-            {
+            while (fib[i] < n)                              //This while loop creates the fibonacci sequence. 'i' both helps with
+            {                                               //the creation of the array and counting the number of places 'n' is at.
                 i++;
                 fib[i] = fib[i - 1] + fib[i - 2];
             }
-            if (fib[i] == n)
+            if (fib[i] == n)                                //if statement to ensure fib[i] equals 'n', and didn't exceed it.
             {
                 Console.WriteLine("place #" + (i + 1));
             }
